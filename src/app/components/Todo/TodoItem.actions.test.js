d03a640327e6eb0e07b099bd31c0e33c878141b2
Renato Benkendorf <renato-benkendorf@hotmail.com>
2017-03-23 10:57:32 -0300

    testing action of remove todo item

--- src/app/components/Todo/TodoItem.actions.test.js
+++ src/app/components/Todo/TodoItem.actions.test.js
@@ -1,4 +1,4 @@
-import { toggleTodoState } from './TodoItem.actions';
+import { toggleTodoState, removeTodoItem } from './TodoItem.actions';
 
 describe('Actions: TodoItemComponent', () => {
     test('should be imported', () => {
@@ -10,6 +10,15 @@ describe('Actions: TodoItemComponent', () => {
         test('should toggle todo item status', () => {
             expect(toggleTodoState(2)).toEqual({
                 type: 'TODO_TOGGLE_DONE',
+                id: 2
+            });
+        });
+    });
+
+    describe('removeTodoItem () =>', () => {
+        test('should remove todo item', () => {
+            expect(removeTodoItem(2)).toEqual({
+                type: 'REMOVE_TODO_ITEM',
                 id: 2
             });
         });
