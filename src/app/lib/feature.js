3279378feb7176c6371f8c02ad14ec6fff54c4e9
Renato Benkendorf <renato-benkendorf@hotmail.com>
2017-03-23 13:54:16 -0300

    improving feature hash browser app for iterator params for generate result test

--- src/app/lib/feature.js
+++ src/app/lib/feature.js
@@ -1,3 +1,9 @@
-export const isEnabled = (name) => {
-    return window.location.hash.split('#')[1] === name;
-};
+export function *isEnabled (names) {
+    let hashes = window.location.hash.split('#');
+    hashes.splice(0, 1);
+    for (let index in names) {
+        if (names.hasOwnProperty(index)) {
+            yield hashes[index] === names[index];
+        }
+    }
+}
